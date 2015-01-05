1573f19a10a8512041756015e95eb3f9e2b6c48e
Soren <soren.erdweg@cern.ch>
2015-01-05 17:32:15 +0100

    (mpl_lib) Moved the test functionallity to seperate test file
    To not have the test functions in the library file, they were moved to
    its own test file. Also a .gitignore file was included to not track
    *.pyc files.

--- lib/DukePlotALot.py
+++ lib/DukePlotALot.py
@@ -12,53 +12,6 @@ import matplotlib.ticker as mticker
 import matplotlib.patches as mpatches
 import matplotlib.lines as mlines
 
-def main():
-    bag_hist, sig_hist, dat_hist = create_test_histos()
-
-    bag_hist.fillstyle = 'solid'
-    bag_hist.fillcolor = 'green'
-    bag_hist.linecolor = 'green'
-    bag_hist.linewidth = 0
-    bag_hist.yaxis.SetTitle('Events')
-    bag_hist.xaxis.SetTitle('Mass (GeV)')
-    
-    sig_hist.fillstyle = '0'
-    sig_hist.fillcolor = 'red'
-    sig_hist.linecolor = 'red'
-    sig_hist.linewidth = 1
-    sig_hist.yaxis.SetTitle('Events')
-    sig_hist.xaxis.SetTitle('Mass (GeV)')
-
-    test = plotter(hist=[bag_hist], sig = [sig_hist],style='CMS')
-    test.Add_data(dat_hist)
-    test.Add_plot('Signi',pos=0, height=15)
-    test.Add_plot('Ratio',pos=1, height=15)
-    test.Add_plot('DiffRatio',pos=2, height=15)
-    test.make_plot('bla_plt.pdf')
-    return 42
-
-def create_test_histos():
-    # set the random seed
-    ROOT.gRandom.SetSeed(42)
-    np.random.seed(42)
-
-    # signal distribution
-    signal = 126 + 10 * np.random.randn(1000)
-    signal_obs = 126 + 10 * np.random.randn(1000)
-
-    # create histograms
-    h1 = Hist(30, 40, 200, title='Background', markersize=0)
-    h2 = h1.Clone(title='Signal')
-    h3 = h1.Clone(title='Data')
-
-    # fill the histograms with our distributions
-    h1.FillRandom('landau', 10000)
-    map(h2.Fill, signal)
-    h3.FillRandom('landau', 10000)
-    map(h3.Fill, signal_obs)
-
-    return h1, h2, h3
-
 class plotter():
     ## Constructor:
     def __init__(self, style = 'Plain', hist = [], sig = [], data_hist = None, data = False):
@@ -452,5 +405,3 @@ class plotter():
         except AttributeError:
             print('No histogram added')
         print('with height: ' + str(self.hist_height) + ' and start: ' + str(self.hist_start))
-
-main()
