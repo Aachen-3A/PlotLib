7561e93be5deca9a1eb14179909513542f4d7c37
Soren <soren.erdweg@cern.ch>
2015-02-24 16:20:31 +0100

    Included the gaussian fit funtion
    A new dunction was included for the object_plotting, it will fit
    a gassian to a given histogram and plot it. This is documented on
    https://github.com/Aachen-3A/PlotLib/wiki/Additional-plotting-objects.

--- lib/object_plotting.py
+++ lib/object_plotting.py
@@ -2,6 +2,7 @@
 
 import sys
 import matplotlib
+import numpy as np
 
 import matplotlib.pyplot as plt
 
@@ -35,7 +36,6 @@ def plot_lower_cut(axis1, value, cut_arrow_position = 0.5):
     axis1.axvline(x = value, color = cut_color, zorder = cut_level, linewidth = 2)
     axis1.arrow((value - xmin) / width, cut_arrow_position, 0.02, 0, head_width = 0.1, head_length = 0.02, fc = cut_color, ec = cut_color, zorder = cut_level, linewidth = 2, transform = axis1.transAxes)
 
-
 def plot_both_cuts(axis1, value1, value2, include = True, cut_arrow_position = 0.5):
     if include:
         if value1 > value2:
@@ -51,3 +51,18 @@ def plot_both_cuts(axis1, value1, value2, include = True, cut_arrow_position = 0
         else:
             plot_lower_cut(axis1, value2, cut_arrow_position = cut_arrow_position)
             plot_upper_cut(axis1, value1, cut_arrow_position = cut_arrow_position)
+
+def gaussian(x, norm, mu, sig):
+    return norm * np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
+
+def plot_gauss_fit(axis1, histo, xmin = 0, xmax = 0):
+    print('Now fitting a gaus to %s and plotting it'%histo.GetTitle())
+    if xmin == 0 and xmax == 0:
+        xmin, xmax = axis1.get_xlim()
+
+    fit_res = histo.Fit('gaus', 'N0S', '', xmin, xmax)
+
+    x = np.linspace(xmin, xmax)
+    y = gaussian(x, fit_res.Parameter(0), fit_res.Parameter(1), fit_res.Parameter(2))
+    line, = axis1.plot(x, y, '-', linewidth=1, zorder = 0)
+
